b474b0e2012ae0ee9010d3ce2471e847deb25fe3
SCabralTenet <scabral@tenet.io>
2024-03-26 17:26:50 -0300

    Add rate limit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const { rateLimit } = require('express-rate-limit');
 
 var app = express();
 
@@ -14,6 +15,12 @@ app.use(cookieParser());
 app.use(cors())
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs')
+
+app.use(rateLimit({  
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // max number of requests allowed
+    message: 'Too many request, please try again later.'
+}));
 
 const indexRouter = require('./src/routes/_index.routes');
 app.use('/', indexRouter);
